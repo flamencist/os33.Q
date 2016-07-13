20f0e6c77add84a0ea4b68c7f717a3252f74135c
unknown <AChermyanin@WKS15.dev3.os33.net>
2016-07-13 11:55:57 +0000

    move q to os33.q

--- q.js
+++ q.js
@@ -35,57 +35,12 @@
     // Common/Node/RequireJS, the module exports the Q API and when
     // executed as a simple <script>, it creates a Q global instead.
 
-/* ================== FIX FOR IE
-    // Montage Require
-    if (typeof bootstrap === "function") {
-        bootstrap("promise", definition);
-
-    // CommonJS
-    } else if (typeof exports === "object" && typeof module === "object") {
-        module.exports = definition();
-
-    // RequireJS
-    } else if (typeof define === "function" && define.amd) {
-        define(definition);
-
-    // SES (Secure EcmaScript)
-    } else if (typeof ses !== "undefined") {
-        if (!ses.ok()) {
-            return;
-        } else {
-            ses.makeQ = definition;
-        }
-
-    // <script>
-    } else
-   
-    if (typeof window !== "undefined" || typeof self !== "undefined") {
-        // Prefer window over self for add-on scripts. Use self for
-        // non-windowed contexts.
-        var global = typeof window !== "undefined" ? window : self;
-
-        // Get the `window` object, save the previous Q global
-        // and initialize Q as a global.
-        var previousQ = global.Q;
-        global.Q = definition();
-
-        // Add a noConflict function so Q can be removed from the
-        // global namespace.
-        global.Q.noConflict = function () {
-            global.Q = previousQ;
-            return this;
-        };
-
-    } else {
-        throw new Error("This environment was not anticipated by Q. Please file a bug.");
-    }
-================================== */ 
     if("undefined"==typeof window){
         throw new Error("This environment was not anticipated by Q. Please file a bug.");
     }else{
      // Prefer window over self for add-on scripts. Use self for
         // non-windowed contexts.
-        var global = window;
+        var global = window.os33 || (window.os33={});
 
         // Get the `window` object, save the previous Q global
         // and initialize Q as a global.
